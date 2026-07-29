de922ffeed73c52716e2ebc28ec0e5981af47a6b
nomaan shaik <nomaanshaiknnomaanshaik@gmail.com>
2026-07-30 00:24:39 +0530

    Add backup Bucket holder

--- portal/storage.py
+++ portal/storage.py
@@ -96,3 +96,96 @@ class SupabaseStorage(Storage):
         return self.supabase.storage.from_(
             self.bucket_name
         ).download(name)
+        
+@deconstructible    
+class SupabaseStorage(Storage):
+
+   
+    def __init__(self):
+
+        self.supabase = create_client(
+            os.getenv("SUPABASE_URL_BACKUP"),
+            os.getenv("SUPABASE_KEY_BACKUP")
+        )
+
+        self.bucket_name = "question-papers-backup"
+
+        # print("SUPABASE URL:", os.getenv("SUPABASE_URL"))
+        # print("BUCKET:", self.bucket_name)
+
+        try:
+            buckets = self.supabase.storage.list_buckets()
+
+            print("AVAILABLE BUCKETS:")
+
+            for bucket in buckets:
+                print(bucket.name)
+
+        except Exception as e:
+            print("BUCKET ERROR:", e)
+
+
+    def _save(self, name, content):
+        try:
+
+            file_data = content.read()
+
+            self.supabase.storage.from_(
+                self.bucket_name
+            ).upload(
+                path=name,
+                file=file_data,
+                file_options={
+                    "content-type": "application/pdf",
+                    # "upsert": "true"
+                }
+            )
+            
+
+            return name
+        except Exception as e:
+            print("UPLOAD ERROR:", repr(e))
+            raise
+
+
+    def exists(self, name):
+
+        try:
+
+            folder = os.path.dirname(name)
+            filename = os.path.basename(name)
+
+            files = self.supabase.storage.from_(
+                self.bucket_name
+            ).list(
+                path=folder
+            )
+
+            for file in files:
+
+                if file.get("name") == filename:
+                    return True
+
+            return False
+
+        except Exception:
+
+            return False
+
+
+    def url(self, name):
+
+        response = self.supabase.storage.from_(
+            self.bucket_name
+        ).create_signed_url(
+            name,
+            3600
+        )
+
+        return response["signedURL"]
+    
+    def download(self, name):
+
+        return self.supabase.storage.from_(
+            self.bucket_name
+        ).download(name)
